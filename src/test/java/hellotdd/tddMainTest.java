094a48e64a568219d6eebce01f2bd516188b27bb
qpakr38 <zczc0819@gmail.com>
2021-12-08 18:00:41 +0900

    Feat: #1 초기세팅

---
+++ src/test/java/hellotdd/tddMainTest.java
@@ -0,0 +1,13 @@
+package hellotdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class tddMainTest {
+    @Test
+    void tddMain() {
+        System.out.println("Hello, Test-Driven Development");
+    }
+
+}
